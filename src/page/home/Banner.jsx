2f24851bab03f56e20e729c822f0425d5d224a9f
mahmudrkb <almahmudrkb98@gmail.com>
2025-02-18 11:57:36 +0600

    home banner update

--- src/page/home/Banner.jsx
+++ src/page/home/Banner.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="mt-16 dark:bg-blue-gray-900   ">
-      <div className='h-screen   bg-fixed   bg-center bg-[url("https://i.ibb.co.com/WPtXRhd/banner2.png")] bg-cover  '>
-        <div className=" flex items-center justify-center   bg-gray-900 bg-opacity-60  h-screen roboto-mono ">
+    <div className="mt-16 dark:bg-blue-gray-900  ">
+      <div className=' max-h-96   bg-fixed   bg-center bg-[url("https://i.ibb.co.com/s9xMqCv7/banner.png")] bg-cover  '>
+        <div className=" flex items-center justify-center   bg-gray-900 bg-opacity-60 h-96 roboto-mono ">
           <div className="text-center space-y-3  text-white">
             <h1 className="text-6xl  "> Find Your Best Friend </h1>
             <p>
